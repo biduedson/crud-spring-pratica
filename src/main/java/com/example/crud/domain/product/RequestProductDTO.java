7b06aaf9fb3e091c28d2bcb6266328fa5c5bd013
Edson <biduzao.bidu21@gmail.com>
2024-03-01 22:11:34 -0300

    fix: replace @Null with @NotNull

--- src/main/java/com/example/crud/domain/product/RequestProductDTO.java
+++ src/main/java/com/example/crud/domain/product/RequestProductDTO.java
@@ -1,6 +1,7 @@
 package com.example.crud.domain.product;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 
 public record RequestProductDTO(
@@ -8,6 +9,6 @@ public record RequestProductDTO(
         String id,
         @NotBlank
         String name,
-        @Null
+        @NotNull
         Integer price_in_cents ) {
 }
